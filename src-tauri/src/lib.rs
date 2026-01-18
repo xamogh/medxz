5b9377db868934aeac0b4b793cd5472996337a74
Amogh Rijal <xamoghx@gmail.com>
2026-01-18 23:08:56 +0545

    feat: add tauri-specta

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,18 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+mod specta_gen;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
+#[specta::specta]
+fn greet(name: String) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let builder = specta_gen::builder();
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(builder.invoke_handler())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src-tauri/src/specta_gen.rs
@@ -0,0 +1,16 @@
+#[cfg(debug_assertions)]
+use specta_typescript::Typescript;
+use tauri_specta::{collect_commands, Builder};
+
+use crate::{greet, __cmd__greet, __specta__fn__greet};
+
+pub fn builder() -> Builder<tauri::Wry> {
+    let builder = Builder::<tauri::Wry>::new().commands(collect_commands![greet]);
+
+    #[cfg(debug_assertions)]
+    builder
+        .export(Typescript::default(), "../src/bindings.ts")
+        .expect("Failed to export typescript bindings");
+
+    builder
+}
