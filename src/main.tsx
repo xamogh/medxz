440a3467dcfda3e58b7ab90223cd5b801a4c7de9
Amogh Rijal <xamoghx@gmail.com>
2026-01-20 23:20:42 +0545

    feat(app): route to dashboard after login

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./app/App";
+import "./app/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
